b5c32f3eb2d721ae1d61485d7bf6296d9f916462
gitchili <jameslphillips@gmail.com>
2018-06-03 23:43:55 -0600

    start of a static nav

--- crypto-meetup/src/App.vue
+++ crypto-meetup/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <v-app>
-    
+   <!-- theme change = dark or light etc -->
+  <v-app dark>
+    <!-- static content  nav, footer, tool bar same for all pages = none dynamic-->
+<v-toolbar>
+  <v-toolbar-title>MeetUP.EOS</v-toolbar-title>
+  <!-- v-spacer v-toolbar uses flexblox-->
+  <v-spacer></v-spacer>
+  <v-toolbar-items>
+    <v-btn>View meetups</v-btn>
+  </v-toolbar-items>
+</v-toolbar>
+    <!-- dynamic content to be displayed lives in a html elemnt named main-->
+    <main>
+      
+    </main>
   </v-app>
 </template>
 
