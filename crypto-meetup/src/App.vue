4487b80930037cc171bece5541a7b98cc75bcec0
gitchili <jameslphillips@gmail.com>
2018-06-05 00:20:05 -0600

    removeing errors & adding an array for nav

--- crypto-meetup/src/App.vue
+++ crypto-meetup/src/App.vue
@@ -2,14 +2,18 @@
  
   <v-app>
     <v-navigation-drawer fixed v-model="sideNav">
-     <v-list>
-       <v-list-tile>
-         <v-list-tile-action>
-           <v-icon>supervisor_account</v-icon>
-         </v-list-tile-action>
-         <v-list-tile-content>View Meetups</v-list-tile-content>
-         </v-list-tile>
-     </v-list>
+      <v-list>
+        <!-- mapping key  to my array & linking -->
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
  <v-toolbar dark class="primary">
       <v-toolbar-side-icon
@@ -18,9 +22,10 @@
       <v-toolbar-title>MeetUp.EOS</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left dark>supervisor_account</v-icon>
-          View Meetups
+        <!-- mapping key the btn -->
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -32,11 +37,19 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        sideNav: false
-      }
+export default {
+  data () {
+    return {
+      sideNav: false,
+      /* menu items stored in a array for my nav */
+      menuItems: [
+        {icon: 'supervisor_account', title: 'View Meetups'},
+        {icon: 'room', title: 'Organize Meetup'},
+        {icon: 'face', title: 'Sign up'},
+        {icon: 'person', title: 'Profile'},
+        {icon: 'lock_open', title: 'Sign in'}
+      ]
     }
   }
+}
 </script>
